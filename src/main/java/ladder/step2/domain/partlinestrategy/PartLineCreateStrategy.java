85e48b0e6493029999706dd64d008a4dca7b8c1d
JunH <tjdtls690@gmail.com>
2022-10-21 15:55:48 +0900

    :art: refactor : 랜덤 생성 시 불필요한 객체 생성 리팩토링

--- src/main/java/ladder/step2/domain/partlinestrategy/PartLineCreateStrategy.java
+++ src/main/java/ladder/step2/domain/partlinestrategy/PartLineCreateStrategy.java
@@ -2,6 +2,10 @@ package ladder.step2.domain.partlinestrategy;
 
 import ladder.step2.domain.PartLine;
 
+import java.util.Random;
+
 public interface PartLineCreateStrategy {
+    Random RANDOM = new Random();
+    
     PartLine create();
 }
--- src/main/java/ladder/step2/domain/partlinestrategy/RandomPartLineCreateStrategy.java
+++ src/main/java/ladder/step2/domain/partlinestrategy/RandomPartLineCreateStrategy.java
@@ -2,15 +2,13 @@ package ladder.step2.domain.partlinestrategy;
 
 import ladder.step2.domain.PartLine;
 
-import java.util.Random;
-
 public class RandomPartLineCreateStrategy implements PartLineCreateStrategy {
     @Override
     public PartLine create() {
         return new PartLine(randomBoolean());
     }
     
-    private static boolean randomBoolean() {
-        return new Random().nextBoolean();
+    private boolean randomBoolean() {
+        return RANDOM.nextBoolean();
     }
 }
